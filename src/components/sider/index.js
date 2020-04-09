811254c3b8f29092ef75ad2f80f1b17696807ae1
Huanqiang <861645007@qq.com>
2020-04-09 10:18:48 +0800

    @dev 修改菜单栏显示状态，刷新后依然能正确显示当前菜单

--- src/components/sider/index.js
+++ src/components/sider/index.js
@@ -1,31 +1,49 @@
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
+import { withRouter } from 'react-router'
 import './index.css'
 
 import routers from '../../router'
 
+/**
+ * 获取菜单显示状态
+ * @param {*} length
+ * @param {*} i
+ */
+const getMenuStatus = (length, i) =>
+  Array(length)
+    .fill(false)
+    .map((item, index) => index === i)
+
+/**
+ * 菜单项组件
+ * @param {*} param0
+ */
 const RouterItem = ({ title, path, isActive, onChange }) => (
-  <li className={isActive ? 'menu-item active' : 'menu-item'} onClick={onChange}>
+  <li
+    className={isActive ? 'menu-item active' : 'menu-item'}
+    onClick={onChange}>
     {/* <div className='background'></div> */}
     <Link to={path}>{title}</Link>
   </li>
 )
 
-export default () => {
-  const [isActives, setIsActives] = useState(Array(routers.length).fill(false))
+export default withRouter(({ location }) => {
+  const [isActives, setIsActives] = useState(
+    getMenuStatus(
+      routers.length,
+      routers.findIndex((router) => router.path === location.pathname)
+    )
+  )
 
   // const isActives =
   const changeActives = (i) => {
     console.log(432432, i)
-    setIsActives(
-      Array(routers.length)
-        .fill(false)
-        .map((item, index) => index === i)
-    )
+    setIsActives(getMenuStatus(routers.length, i))
   }
 
   return (
-    <ul className='menu'>
+    <ul className="menu">
       {routers.map((router, index, array) => (
         <RouterItem
           key={router.title}
@@ -36,4 +54,4 @@ export default () => {
       ))}
     </ul>
   )
-}
+})
