a467a47267de1f091524fc29f037187b2e4b08df
Huanqiang <huanqiang124@163.com>
2020-05-05 16:32:19 +0800

    @dev test

---
+++ script/deploy.sh
@@ -0,0 +1,10 @@
+echo "开始编译"
+npm run build
+
+echo "开始 git 操作"
+git init
+git add .
+git commit -m "@dev test"
+git remote add origin https://github.com/Huanqiang/Huanqiang.github.io.git
+git pull origin master --allow-unrelated-histories
+git push
