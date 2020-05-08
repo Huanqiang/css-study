61fe529ad0e9a7c5775d9c2a81ac8992510bbccf
Huanqiang <huanqiang124@163.com>
2020-05-08 11:54:56 +0800

    Updates

---
+++ precache-manifest.d43d02190cefde4d41928db4225c43d0.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ff57d93bff545b06d8f2e5923695bc2",
+    "url": "/Huanqiang/Huanqiang.github.io/index.html"
+  },
+  {
+    "revision": "c8fca07ef201d2b85937",
+    "url": "/Huanqiang/Huanqiang.github.io/static/css/2.0b03e344.chunk.css"
+  },
+  {
+    "revision": "fa1d9277a90505e42326",
+    "url": "/Huanqiang/Huanqiang.github.io/static/css/main.b1ee84eb.chunk.css"
+  },
+  {
+    "revision": "c8fca07ef201d2b85937",
+    "url": "/Huanqiang/Huanqiang.github.io/static/js/2.69a1d11c.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Huanqiang/Huanqiang.github.io/static/js/2.69a1d11c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa1d9277a90505e42326",
+    "url": "/Huanqiang/Huanqiang.github.io/static/js/main.8a37ff12.chunk.js"
+  },
+  {
+    "revision": "58388ca3ce3d69ee3c9c",
+    "url": "/Huanqiang/Huanqiang.github.io/static/js/runtime-main.ef1b4ab7.js"
+  },
+  {
+    "revision": "aa77d1d0717825f61c8ec9589b2e8191",
+    "url": "/Huanqiang/Huanqiang.github.io/static/media/avatar.aa77d1d0.JPG"
+  },
+  {
+    "revision": "01efbc18feba24452415b375e54ba83f",
+    "url": "/Huanqiang/Huanqiang.github.io/static/media/bg.01efbc18.jpg"
+  },
+  {
+    "revision": "0177418d8badb9dd65d42c701296f978",
+    "url": "/Huanqiang/Huanqiang.github.io/static/media/index.0177418d.md"
+  }
+]);
---
+++ static/media/index.0177418d.md
@@ -0,0 +1,63 @@
+# 我的博客
+
+这是一个 markdown 的页面
+
+## 列表测试
+
+### 标题 3.1.1
+
+1. 有序列表 1
+2. 有序列表 2
+
+- 无序列表 1
+- 无序列表 2
+- 无序列表 3
+- 无序列表 4
+
+### 标题 3.1.2
+
+#### 标题 4.1.2.1
+
+## 代码测试
+
+### 标题 3.2.1
+
+```js
+const func = () => {
+  console.log('This is a js function')
+}
+```
+
+code Test
+
+```js
+function $initHighlight(block, cls) {
+  try {
+    if (cls.search(/\bno\-highlight\b/) != -1)
+      return process(block, true, 0x0F) +
+             ` class="${cls}"`;
+  } catch (e) {
+    /* handle exception */
+  }
+  for (var i = 0 / 2; i < classes.length; i++) {
+    if (checkCondition(classes[i]) === undefined)
+      console.log('undefined');
+  }
+
+  return (
+    <div>
+      <web-component>{block}</web-component>
+    </div>
+  )
+}
+
+export  $initHighlight;
+```
+
+## 其他
+
+### 标题 3.3.1
+
+#### 标题 4.3.1.1
+
+> 感谢 https://juejin.im/post/5e070cd9f265da33f8653f00
